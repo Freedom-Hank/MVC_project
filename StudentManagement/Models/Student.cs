4d3c718546c36bd6f31435dcf38142b14d40fb0b
HW4 <magicss0914@gmail.com>
2024-11-20 16:56:55 +0800

    更新資料庫顯示

--- StudentManagement/Models/Student.cs
+++ StudentManagement/Models/Student.cs
@@ -4,7 +4,8 @@
     {
         public int StudentId { get; set; }
         public string Name { get; set; }
-        public int Grade { get; set; }
+        public int? StudentNumber { get; set; }
+        public int? Grade { get; set; }
         // 添加更多屬性，如 Email、Phone 等
     }
 }
