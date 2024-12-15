42bbc9e47a6e9f1ad45fc3217fd184f3762e01a3
0113-xuan <magicss0914@gmail.com>
2024-12-15 22:14:33 +0800

    新增選課資料

--- StudentManagement/Models/CourseRegistration.cs
+++ StudentManagement/Models/CourseRegistration.cs
@@ -3,5 +3,14 @@
     public class CourseRegistration
     {
         public int CourseId { get; set; }
+        public string Class { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public int Credit { get; set; }
+        public string Instructor {  get; set; }
+        public string Schedule { get; set; }
+        public string Location { get; set; }
+        public string Semester { get; set; }
+
     }
 }
