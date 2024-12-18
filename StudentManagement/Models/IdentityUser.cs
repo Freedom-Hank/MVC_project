9fb9f69198b456dcb1a77c96898fbeedf2163693
Freedom-Hank <hank3192@gmail.com>
2024-12-18 20:35:23 +0800

    新增model類別 IdentityUser.cs

---
+++ StudentManagement/Models/IdentityUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StudentManagement.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string StudentName { get; set; }
+        public string StudentNumber { get; set; }
+        public int Grade { get; set; }
+        public char? Class { get; set; }
+        public string Gender { get; set; }
+        public string Nationality { get; set; }
+    }
+}
