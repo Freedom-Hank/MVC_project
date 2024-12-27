dbd3e4fb00e36030b9de6199f18ce98997cad58d
Freedom-Hank <hank3192@gmail.com>
2024-12-27 17:05:10 +0800

    設定成登入才能存取歷屆修課

--- StudentManagement/Controllers/CourselistController.cs
+++ StudentManagement/Controllers/CourselistController.cs
@@ -6,6 +6,14 @@ namespace StudentManagement.Controllers
     {
         public IActionResult Index()
         {
+            // 檢查使用者是否已登入
+            if (HttpContext.Session.GetString("LoggedInStudentNumber") == null)
+            {
+                // 如果未登入，重定向到登入頁面
+                return RedirectToAction("Login", "Account");
+            }
+
+            // 如果已登入，顯示修課列表
             return View();
         }
     }
